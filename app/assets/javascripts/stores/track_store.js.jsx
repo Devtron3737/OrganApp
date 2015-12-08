c570e6e44103a7afa863ce97ee3a19834698a022
Devtron718 <devin.miranda@gmail.com>
2015-12-07 17:06:35 -0800

    add getter setter methods

--- app/assets/javascripts/stores/track_store.js.jsx
+++ app/assets/javascripts/stores/track_store.js.jsx
@@ -6,11 +6,31 @@
     {},
     EventEmitter.prototype,
     {
-      // dispatcherId: Dispatcher.register( function (payLoad) {
-      //   switch (payLoad.actionType) {
-      //     case
-      //   }
-      // })
+      dispatcherId: Dispatcher.register( function (payLoad) {
+        switch (payLoad.actionType) {
+          case "update_tracks":
+            TrackStore.updateTracks(payLoad.tracks)
+            break;
+        }
+      }),
+
+      getTracks: function () {
+        return _tracks.slice()
+      },
+
+      updateTracks: function (tracks) {
+        _tracks = tracks;
+
+        this.emit(TRACK_CHANGE)
+      },
+
+      addTrackChangeListener: function (callback) {
+        this.on(TRACK_CHANGE, callback)
+      },
+
+      removeTrackChangeListener: function (callback) {
+        this.removeListener(TRACK_CHANGE, callback)
+      }
     }
   )
 })(this)
